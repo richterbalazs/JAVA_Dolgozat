1d9aea4111492126c186509ae851b1e0107cbd79
Richter Balázs Krisztián <richter.b.krisztian@dk.akkszalezi.hu>
2024-12-09 14:45:54 +0100

    f1 7

--- src/main/java/szamalk/hu/Main.java
+++ src/main/java/szamalk/hu/Main.java
@@ -6,5 +6,6 @@ public class Main {
     public static void main(String[] args) {
         Tabla tabla = new Tabla("8", "#");
         tabla.megjelenit();
+        tabla.elhelyez(5);
     }
 }
--- src/main/java/szamalk/hu/modell/Tabla.java
+++ src/main/java/szamalk/hu/modell/Tabla.java
@@ -1,19 +1,20 @@
 package szamalk.hu.modell;
 
+import java.util.Arrays;
+
 public class Tabla {
     private String[][] T;
     private String UresCella;
 
     public Tabla(String T, String UresCella) {
-        this.T = new String[8][1];
+        this.T = new String[8][8];
         this.UresCella = UresCella;
 
     }
 
-    public void megjelenit(){
+    public void Tabla(){
         T = new String[][]{
-                {UresCella, UresCella, UresCella, UresCella, UresCella, UresCella, UresCella, UresCella},
-
+                {UresCella, UresCella, UresCella, UresCella, UresCella, UresCella, UresCella, UresCella}
         };
 
         for (int i = 0; i < T.length; i++){
@@ -22,10 +23,50 @@ public class Tabla {
             }
             System.out.println();
         }
+
+        elhelyez(5);
     }
 
-    public void elhelyez(){
+    public void megjelenit(){
+        Tabla();
+    }
+
+    public void elhelyez(int N){
+        String K = "K";
+        for (int i = 0; i < T.length; i++){
+            if(T[i] == null){
+                T += K;
+            }
+        }
 
     }
 
+    public boolean UresOszlop(int oszlop){
+        boolean telivan = true;
+
+        for(int i = 0; i < T.length; i++) {
+            if(T[i][oszlop] != UresCella){
+                telivan = false;
+            }
+        }
+        return telivan;
+    }
+
+    public void UresSor(int sor){
+        boolean telivan = true;
+
+        for(int i = 0; i < T.length; i++) {
+            if(T[i][sor] != UresCella){
+                telivan = false;
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Tabla{" +
+                "T=" + Arrays.toString(T) +
+                ", UresCella='" + UresCella + '\'' +
+                '}';
+    }
 }
