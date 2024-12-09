2a49a7735281a8682518e2f3fc5f940f53edb568
Richter Balázs Krisztián <richter.b.krisztian@dk.akkszalezi.hu>
2024-12-09 13:27:06 +0100

    f1 1-2

--- src/main/java/szamalk/hu/Main.java
+++ src/main/java/szamalk/hu/Main.java
@@ -2,6 +2,6 @@ package szamalk.hu;
 
 public class Main {
     public static void main(String[] args) {
-        
+
     }
 }
---
+++ src/main/java/szamalk/hu/modell/Tabla.java
@@ -0,0 +1,8 @@
+package szamalk.hu.modell;
+
+public class Tabla {
+    private String[][] T=new String[3][8];
+    private String UresCella;
+
+    
+}
