5cd1a193d8d918c74bb64f59d712d5200fe8a360
Richter Balázs Krisztián <richter.b.krisztian@dk.akkszalezi.hu>
2024-12-09 13:38:26 +0100

    f1 1-2 javitva

--- src/main/java/szamalk/hu/modell/Tabla.java
+++ src/main/java/szamalk/hu/modell/Tabla.java
@@ -1,8 +1,18 @@
 package szamalk.hu.modell;
 
 public class Tabla {
-    private String[][] T=new String[3][8];
+    private String[][] T;
     private String UresCella;
 
-    
+    public Tabla(String karakter) {
+        this.T = new String[8][8];
+        UresCella = karakter;
+        for(int i = 0; i < 0; i++){
+            for (int j = 0; j < 0; j++){
+                T[i][j] = karakter;
+            }
+        }
+    }
+
+
 }
