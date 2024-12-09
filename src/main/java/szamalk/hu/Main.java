2b477ed69d5f02d1e8dab835f8cf6ce1400eb460
Richter Balázs Krisztián <richter.b.krisztian@dk.akkszalezi.hu>
2024-12-09 14:20:38 +0100

    f1 4

--- src/main/java/szamalk/hu/Main.java
+++ src/main/java/szamalk/hu/Main.java
@@ -1,7 +1,10 @@
 package szamalk.hu;
 
+import szamalk.hu.modell.Tabla;
+
 public class Main {
     public static void main(String[] args) {
-
+        Tabla tabla = new Tabla("8", "#");
+        tabla.megjelenit();
     }
 }
--- src/main/java/szamalk/hu/modell/Tabla.java
+++ src/main/java/szamalk/hu/modell/Tabla.java
@@ -4,15 +4,28 @@ public class Tabla {
     private String[][] T;
     private String UresCella;
 
-    public Tabla(String karakter) {
-        this.T = new String[8][8];
-        UresCella = karakter;
-        for(int i = 0; i < 0; i++){
-            for (int j = 0; j < 0; j++){
-                T[i][j] = karakter;
+    public Tabla(String T, String UresCella) {
+        this.T = new String[8][1];
+        this.UresCella = UresCella;
+
+    }
+
+    public void megjelenit(){
+        T = new String[][]{
+                {UresCella, UresCella, UresCella, UresCella, UresCella, UresCella, UresCella, UresCella},
+
+        };
+
+        for (int i = 0; i < T.length; i++){
+            for(int j = 0; j < T[i].length; j++){
+                System.out.println(T[i][j]);
             }
+            System.out.println();
         }
     }
 
+    public void elhelyez(){
+
+    }
 
 }
